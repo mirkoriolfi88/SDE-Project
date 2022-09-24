3dc0c47284c815d22705f7fc5884469825d9e5be
mirkoriolfi88 <tartaruga88@gmail.com>
2022-09-24 16:01:55 +0200

    Initial commit

---
+++ SDE-Project/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SDE_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet(Name = "GetTestValue")]
+        public string Get()
+        {
+            return "Test new controller";
+        }
+    }
+}
